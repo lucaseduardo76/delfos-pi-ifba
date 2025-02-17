2ad3c059bceefd5bdda6e22c624c9a96290dbb04
Joran <joshwa315@gmail.com>
2025-02-17 18:17:30 -0300

    Fix: consertando o gif de cadastro confirmado

--- public/js/closeModal.js
+++ public/js/closeModal.js
@@ -1,25 +1,3 @@
-document.getElementById("closeModal").addEventListener("click", () => {
-    document.getElementById("modal").style.opacity = "0";
-
-    const timer = setTimeout(() => {
-        document.getElementById("modalLogin").style.display = "none";
-    }, 450);
-});
-
-document.getElementById("loginButton").addEventListener("click", () => {
-    document.getElementById("modalLogin").style.display = "flex";
-
-    const timer = setTimeout(() => {
-        document.getElementById("modal").style.opacity = "1";
-    }, 10);
-});
-
-
-document.getElementById("closeModalBtn").addEventListener("click", () => {
-    document.getElementById("popup").style.display = "none";
-});
-
-
 document.addEventListener("DOMContentLoaded", function () {
     const cpfInput = document.querySelector('input[name="cpf"]');
     const telefoneInput = document.querySelector('input[name="telefone"]');
